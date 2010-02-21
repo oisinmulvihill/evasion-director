a0ac23e1d4f12f4781c82552a6a5bbca4a37bf8d
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2010-02-21 23:00:22 +0000

    worked on controller start/stop signaling which I can then use in webadmin to start+stop any controller process.

--- lib/director/signals.py
+++ lib/director/signals.py
@@ -23,11 +23,15 @@
 import uuid
 import pprint
 import logging
+import traceback
 
 from pydispatch import dispatcher
 
 import messenger
 import director.config
+
+# Default time in seconds before raising timeout:
+DEFAULT_TIMEOUT = 60
 
 
 class SignalTimeout(Exception):
@@ -65,13 +69,13 @@ class SignalsSender(object):
         """
         token = str(uuid.uuid4())
         self.log.debug("ping: waiting for director with token '%s'." % (token))
-        evt = messenger.EVT("EVT_DIRECTOR_PING")
+        sig = messenger.EVT("EVT_DIRECTOR_PING")
         if testing:
             # use the unittest's EVT_DIRECTOR_PING instead.
-            evt = testing            
+            sig = testing            
         try:
-            rc = messenger.send_await(evt, token, timeout=timeout)
-            rtoken = rc['data']
+            rc = messenger.send_await(sig, token, timeout=timeout)
+            rtoken = rc['data']['data']
             if token != rtoken:
                 self.log.warn("ping: received ping token '%s' is different to '%s'" % (token, rtoken))
             else:
@@ -80,6 +84,104 @@ class SignalsSender(object):
         except messenger.EventTimeout, e:
             raise SignalTimeout("Director presence check failed! Is it running?")        
 
+
+    def controllerState(self, timeout=DEFAULT_TIMEOUT):
+        """
+        Called to return the state of all controllers listed in
+        the configuration. 
+
+        :param timeout: This is the amount of time (in seconds) 
+        used to wait for the director to respond. If a response 
+        isn't received within this then SignalTimeout will be 
+        raised.
+        
+        :returns: See SignalReceiver controllerState.
+
+        Event Dispatched: EVT_DIRECTOR_CTRLSTATE
+
+        """
+        sig = messenger.EVT("EVT_DIRECTOR_CTRLSTATE")
+        try:
+            self.log.debug("controllerState: (request sig id %s) getting controller state." % sig.uid)
+            rc = messenger.send_await(sig)
+
+        except messenger.EventTimeout, e:
+            self.log.error("controllerState: (request sig id %s) event timeout!" % sig.uid)
+            raise SignalTimeout("Director communication timeout (%s)s! Is it running?" % timeout)
+
+        else:
+            self.log.debug("controllerState: (reply for sig id %s) recovered controller state ok." % sig.uid)
+            rc = rc['data']
+        
+        return rc    
+
+
+    def controllerStart(self, name, timeout=DEFAULT_TIMEOUT):
+        """
+        Called to tell a controller to start running. 
+
+        :param name: This will contain the name of the controller 
+        to be started. This name is the same name as that in the 
+        configuration section.
+        
+        :param timeout: This is the amount of time (in seconds) 
+        used to wait for the director to respond. If a response 
+        isn't received within this then SignalTimeout will be 
+        raised.
+        
+        :returns: See SignalReceiver controllerStart.
+
+        Event Dispatched: EVT_DIRECTOR_CTRLSTART
+
+        """
+        sig = messenger.EVT("EVT_DIRECTOR_CTRLSTART")
+        try:
+            self.log.debug("controllerStart: (request sig id %s) getting controller state." % sig.uid)
+            rc = messenger.send_await(sig, name)
+
+        except messenger.EventTimeout, e:
+            self.log.error("controllerStart: (request sig id %s) event timeout!" % sig.uid)
+            raise SignalTimeout("Director communication timeout (%s)s! Is it running?" % timeout)
+
+        else:
+            self.log.debug("controllerStart: (reply for sig id %s) recovered controller state ok." % sig.uid)
+            rc = rc['data']
+        
+        return rc    
+
+
+    def controllerStop(self, name, timeout=DEFAULT_TIMEOUT):
+        """
+        Called to tell a controller to stop running. 
+
+        :param name: This will contain the name of the controller 
+        to be started. This name is the same name as that in the 
+        configuration section.
+        
+        :param timeout: This is the amount of time (in seconds) 
+        used to wait for the director to respond. If a response 
+        isn't received within this then SignalTimeout will be 
+        raised.
+        
+        :returns: See SignalReceiver controllerStart.
+
+        Event Dispatched: EVT_DIRECTOR_CTRLSTOP
+
+        """
+        sig = messenger.EVT("EVT_DIRECTOR_CTRLSTOP")
+        try:
+            self.log.debug("controllerStop: (request sig id %s) getting controller state." % sig.uid)
+            rc = messenger.send_await(sig, name)
+
+        except messenger.EventTimeout, e:
+            self.log.error("controllerStop: (request sig id %s) event timeout!" % sig.uid)
+            raise SignalTimeout("Director communication timeout (%s)s! Is it running?" % timeout)
+
+        else:
+            self.log.debug("controllerStop: (reply for sig id %s) recovered controller state ok." % sig.uid)
+            rc = rc['data']
+        
+        return rc    
 
 
 class SignalsReceiver(object):
@@ -100,6 +202,32 @@ class SignalsReceiver(object):
         self.manager = manager
 
 
+    def resultDict(self, data, result='ok'):
+        """
+        Return an empty signal response which will be filled out
+        
+        :param result: 'ok' or 'error'
+        :param data: any given data type that can be pickled.
+        
+        :returns: dict(result=result, data=data)
+        
+        """
+        result = result.lower()
+        
+        if result not in ['ok','error']:
+            raise ValueError("Incorrect result value '%s'." % result)
+            
+        return dict(result=result, data=data)
+
+
+    def formatError(self):
+        """Return a string representing the last traceback.
+        """
+        exception, instance, tb = traceback.sys.exc_info()
+        error = "".join(traceback.format_tb(tb))      
+        return error
+
+
     def signalPing(self, signal, sender, **data):
         """
         Called to handle the EVT_DIRECTOR_PING signal, which is
@@ -111,10 +239,194 @@ class SignalsReceiver(object):
         called then this will be a uuid string. The ping function
         will warn if the same uuid is not returned.
         
+        :returns: a call result dict.
+            
+            rc['result'] = 'ok'
+            
+            rc['data'] = the token we were given.
+            
         """
         rtoken = data['data']
         self.log.debug("main: EVT_DIRECTOR_PING received token '%s' replying with same." % (rtoken))
-        messenger.reply(signal, rtoken)
+        messenger.reply(signal, self.resultDict(rtoken))
+
+
+    def signalControllerState(self, signal, sender, **data):
+        """
+        Called to handle the EVT_DIRECTOR_CTRLSTATE signal, which 
+        is used to return the state of all controllers listed in
+        the configuration. 
+        
+        :param: None
+        
+        :returns: a call result dict.
+            
+            rc['result'] = 'ok' | 'error'
+        
+            For ok result:
+            
+            rc['data'] = [
+                dict(name='..name..', disabled='no'|'yes', started=False | True, config={...}),
+                :
+                etc
+            ]
+            
+            For error result:
+            
+            rc['data'] = 'Error / Exception message'
+
+        Note: this will contain every controller except the 
+        director controller.
+            
+        Event Dispatched: EVT_DIRECTOR_CTRLSTATE
+
+        """
+        controller_state = []
+        try:
+            self.log.debug("controllerState: received request (sig id %s)." % signal.uid)
+            
+            c = director.config.get_cfg()
+            
+            for ctrl in c.cfg:
+                if ctrl.name == 'director':
+                    # skip the director as its not a normal controller.
+                    continue
+                
+                # Recover the config for just this controller:
+                ctrl_config = {}
+                if ctrl.config:
+                    ctrl_config = ctrl.config
+                
+                # Check if the controller module is present and actually running.
+                started = False
+                if ctrl.mod:
+                    started = ctrl.mod.isStarted()
+            
+                controller_state.append(dict(
+                    name=ctrl.name,
+                    disabled=ctrl.disabled,
+                    started=started,
+                    config=ctrl_config,
+                ))
+            
+        except:
+            self.log.exception("controllerState: error handling signal (sig id %s) - " % signal.uid)
+            msg = self.formatError()
+            rc = self.resultDict(msg, 'error')
+
+        else:
+            rc = self.resultDict(controller_state)
+
+        self.log.debug("controllerState: replying to request (sig id %s) - " % signal.uid)
+        messenger.reply(signal, rc)
+
+
+    def signalControllerStart(self, signal, sender, **data):
+        """
+        Called to handle the EVT_DIRECTOR_CTRLSTART signal, which 
+        is used to return the result of a start command. 
+        
+        :param data['data]: This will contain the name of the
+        controller to be started. This name is the same name
+        as that in the configuration section.
+        
+        :returns: a call result dict.
+            
+            rc['result'] = 'ok' | 'error'
+        
+            For ok result:
+            
+            rc['data'] = ''
+            
+            For error result:
+            
+            rc['data'] = 'Error / Exception message'
+
+        Note: this will contain every controller except the 
+        director controller.
+            
+        Event Dispatched: EVT_DIRECTOR_CTRLSTART
+
+        """
+        name = data['data']
+        msg = ''
+        try:
+            self.log.debug("signalControllerStart: received request (sig id %s)." % signal.uid)
+            
+            c = director.config.get_cfg()
+            for ctrl in c.cfg:
+                if ctrl.name == name:
+                    if ctrl.disabled == "yes":
+                        msg = "The controller '%s' is disabled and cannot be started" % name
+                    else:
+                        ctrl.mod.start()
+                    
+                    break
+
+        except:
+            self.log.exception("signalControllerStart: error handling signal (sig id %s) - " % signal.uid)
+            msg = self.formatError()
+            rc = self.resultDict(msg, 'error')
+
+        else:
+            rc = self.resultDict(msg)
+
+        self.log.debug("signalControllerStart: replying to request (sig id %s) - " % signal.uid)
+        messenger.reply(signal, rc)
+
+
+    def signalControllerStop(self, signal, sender, **data):
+        """
+        Called to handle the EVT_DIRECTOR_CTRLSTOP signal, which 
+        is used to return the result of a stop command. 
+        
+        :param data['data]: This will contain the name of the
+        controller to be started. This name is the same name
+        as that in the configuration section.
+        
+        :returns: a call result dict.
+            
+            rc['result'] = 'ok' | 'error'
+        
+            For ok result:
+            
+            rc['data'] = ''
+            
+            For error result:
+            
+            rc['data'] = 'Error / Exception message'
+
+        Note: this will contain every controller except the 
+        director controller.
+            
+        Event Dispatched: EVT_DIRECTOR_CTRLSTOP
+
+        """
+        name = data['data']
+        msg = ''
+        try:
+            self.log.debug("signalControllerStop: received request (sig id %s)." % signal.uid)
+    
+            c = director.config.get_cfg()
+            for ctrl in c.cfg:
+                if ctrl.name == name:
+                    if ctrl.disabled == "yes":
+                        msg = "The controller '%s' is disabled and cannot be stopped" % name
+                    else:
+                        ctrl.mod.stop()
+                    
+                    break
+
+        except:
+            self.log.exception("signalControllerStop: error handling signal (sig id %s) - " % signal.uid)
+            msg = self.formatError()
+            rc = self.resultDict(msg)
+
+        else:
+            rc = self.resultDict(msg)
+
+        self.log.debug("signalControllerStop: replying to request (sig id %s) - " % signal.uid)
+        messenger.reply(signal, rc)
 
 
     def signalExit(self, signal, sender, **data):
@@ -124,6 +436,12 @@ class SignalsReceiver(object):
         
         """
         self.log.warn("main: EVT_EXIT_ALL received, exiting...")
+        rc = self.resultDict('ok')
+        messenger.reply(signal, rc)
+        
+        # allow time for message to reply of make its 
+        # way out of director process if needs be.
+        time.sleep(2)
         self.manager.exit()
 
 
@@ -140,7 +458,21 @@ class SignalsReceiver(object):
             signal=messenger.EVT("EVT_DIRECTOR_PING")
         )        
         
-        self.log.info("signalSetup: EVT_EXIT_ALL signal setup.")
+        dispatcher.connect(
+            self.signalControllerState,
+            signal=messenger.EVT("EVT_DIRECTOR_CTRLSTATE")
+        )        
+        
+        dispatcher.connect(
+            self.signalControllerStart,
+            signal=messenger.EVT("EVT_DIRECTOR_CTRLSTART")
+        )        
+        
+        dispatcher.connect(
+            self.signalControllerStop,
+            signal=messenger.EVT("EVT_DIRECTOR_CTRLSTOP")
+        )        
+        
         # Register messenger hook for shutdown()
         dispatcher.connect(
             self.signalExit,
--- lib/director/tests/testdirector.py
+++ lib/director/tests/testdirector.py
@@ -1,16 +1,225 @@
 """
 """
+import os
+import sys
 import pprint
+import os.path
 import unittest
+
+import tempfile
 
 import director
 import messenger
-from director.signals import SignalsSender
+from director import signals
 from messenger.testing import message_main
 from director.testing import director_setup
 
 
 class DirectorTC(unittest.TestCase):
+
+    def cleanUp(self, top):
+        for root, dirs, files in os.walk(top, topdown=False):
+            for name in files:
+                #print 'name:',name
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                #print 'dir name:',name
+                os.rmdir(os.path.join(root, name))
+
+
+    def makeController(self, my_controller):
+        p = tempfile.mkdtemp()
+        sys.path.append(p)
+        
+        # Create a controller I can play with and test
+        # the director behaviour with.
+        #
+        mypkg = os.path.join(p,'mypackage')
+        os.mkdir(mypkg)
+        
+        f = os.path.join(mypkg, '__init__.py')
+        fd = open(f, 'wb')
+        fd.write("\n")
+        fd.close()
+        
+        # Create an agent module that import_module should find and load.
+        #
+        f = os.path.join(mypkg, 'mycontroller.py')
+        fd = open(f, 'wb')
+        fd.write(my_controller)
+        fd.close()
+        
+        return p
+
+
+    def testControllerStartStopRestart(self):
+        """Test a ping signal to the director.
+        """
+        my_controller = r"""
+from director.controllers import base
+
+class Controller(base.Controller):
+
+    def setUp(self, config):
+        base.Controller.setUp(self, config)
+        self.startCalled = False
+        self.stopCalled = False
+        self.tearDownCalled = False
+        self.isStartedCheck = False
+        self.isStoppedCheck = False
+        self.extraArg = config.get('extra_arg')
+
+    def start(self):
+        self.startCalled = True
+
+    def isStarted(self):
+        return self.isStartedCheck
+
+    def stop(self):
+        self.stopCalled = True
+
+    def isStopped(self):
+        return self.isStoppedCheck
+
+    def tearDown(self):
+        self.tearDownCalled = True
+
+        """
+        pkg_path = self.makeController(my_controller)
+        
+        test_config = """
+        [director]
+        msg_host = '%(broker_interface)s'
+        msg_port = %(broker_port)s
+        msg_username = ''
+        msg_password = ''
+        msg_channel = 'evasion'
+        msg_interface = '%(broker_interface)s'
+        proxy_dispatch_port = %(proxy_port)s
+        
+        # sets up a broker running when twisted runs:
+        internal_broker = 'yes'
+        
+        [mycontroller]
+        #disabled = 'yes'
+        order = 8
+        controller = 'mypackage.mycontroller'
+        extra_arg = 'hello there'
+        
+        """
+        m = director_setup(test_config)
+        
+        def testmain(tc):
+            """Start-Stop-Restart"""
+            # The configuration should contain the director and
+            # controller with the load module instance from our
+            # test controller
+            #
+            c = director.config.get_cfg()
+            self.assertNotEquals(c.director, None)
+            self.assertEquals(len(c.cfg), 2)
+            
+            # Quick ping to see if messaging is up and running:
+            d = signals.SignalsSender()
+            d.ping()
+            
+            # Check the initial state of our test controller. Its
+            # loaded under a partial director. This is missing the
+            # start although each controller will have had its setUp
+            # called. We'll take card of running the controller via
+            # signalling.
+            #
+            ctrl = c.cfg[1]
+            self.assertEquals(ctrl.disabled, 'no')
+            self.assertEquals(ctrl.order, 8)
+            self.assertEquals(ctrl.controller, 'mypackage.mycontroller')
+            self.assertNotEquals(ctrl.mod, None)
+            self.assertEquals(ctrl.extra_arg, 'hello there')
+            
+            self.assertEquals(ctrl.mod.startCalled, False)
+            self.assertEquals(ctrl.mod.stopCalled, False)
+            self.assertEquals(ctrl.mod.tearDownCalled, False)
+            self.assertEquals(ctrl.mod.isStartedCheck, False)
+            self.assertEquals(ctrl.mod.isStoppedCheck, False)
+            self.assertEquals(ctrl.mod.extraArg, 'hello there')
+
+            def err_msg(correct, rc):
+                return """rc != correct
+                
+                correct:
+                %s
+                
+                rc:
+                %s
+                
+                """ % (pprint.pformat(correct),pprint.pformat(rc))
+
+
+            # Recover the current state of the controllers:
+            #
+            print "Calling controllerState..."
+            rc = d.controllerState()
+            self.assertEquals(rc['result'], 'ok')
+            
+            correct = [
+                # The director won't appear in this list although its technically 
+                # a controller.
+                dict(name='mycontroller', disabled='no', started=False, config=ctrl.mod.config),
+            ]
+            
+            self.assertEquals(len(rc['data']), 1)
+            self.assertEquals(rc['data'], correct, err_msg(correct, rc['data']))
+
+
+            # Tell the controller to start and check it is:
+            #
+            print "Calling controllerStart..."
+            rc = d.controllerStart('mycontroller')
+            self.assertEquals(rc['result'], 'ok', rc['data'])
+            
+            print "Calling controllerState..."
+            rc = d.controllerState()
+            self.assertEquals(rc['result'], 'ok')
+            
+            correct = [
+                dict(name='mycontroller', disabled='no', started=True, config=ctrl.mod.config),
+            ]
+            
+            self.assertEquals(rc['data'], correct, err_msg(correct, rc['data']))
+            
+            c = director.config.get_cfg()
+            self.assertEquals(ctrl.mod.startCalled, True)
+            self.assertEquals(ctrl.mod.stopCalled, False)
+
+
+            # Tell the controller to stop and check it is:
+            #
+            print "Calling controllerStop..."
+            rc = d.controllerStart('mycontroller')
+            self.assertEquals(rc['result'], 'ok')
+            
+            print "Calling controllerState..."
+            rc = d.controllerState()
+            self.assertEquals(rc['result'], 'ok')
+            
+            correct = [
+                dict(name='mycontroller', disabled='no', started=True, config=ctrl.mod.config),
+            ]
+            
+            self.assertEquals(rc['data'], correct, err_msg(correct, rc['data']))
+            
+            c = director.config.get_cfg()
+            self.assertEquals(ctrl.mod.startCalled, True)
+            self.assertEquals(ctrl.mod.stopCalled, True)
+            
+
+        try:
+            # Run inside the messaging system:
+            message_main(self, testmain, cfg=messenger.default_config['stomp'])
+            
+        except:
+            self.cleanUp(pkg_path)
+            raise
 
 
     def testPingSignal(self):
@@ -34,7 +243,7 @@ class DirectorTC(unittest.TestCase):
         
         def testmain(tc):
             """This should ping without raising a timeout exceptions"""
-            d = SignalsSender()
+            d = signals.SignalsSender()
             d.ping()
        
             
